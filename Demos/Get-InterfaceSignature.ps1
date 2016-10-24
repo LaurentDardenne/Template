3fe1e017a365913d14159043db33d88af49b8566
LaurentDardenne <LaDardenne@Orange.fr>
2016-10-24 12:59:05 +0200

    Change the string formatting

--- Demos/Get-InterfaceSignature.ps1
+++ Demos/Get-InterfaceSignature.ps1
@@ -7,18 +7,18 @@
  )
 
  process {
-  Write-debug "Typename : $TypeName"
+  Write-debug "TypeName : $TypeName"
 
   [System.Type]$Type=$null
   if (-not [System.Management.Automation.LanguagePrimitives]::TryConvertTo($TypeName,[Type],[ref] $Type))
   {
-    $Exception=New-Object System.ArgumentException('Le type est inconnu.','TypeName')
+    $Exception=New-Object System.ArgumentException('Unknown type.','TypeName')
     Write-Error -Exception $Exception
     return
   }
   if (-not $Type.isInterface)
   {
-    Write-Warning "Le type $Type n'est pas une interface."
+    Write-Warning "The type $Type is not an interface."
     return
   }
   $Members=$Type.GetMembers()
@@ -30,7 +30,7 @@
     #les méthodes suffisent à l'implémentation de la propriété
     #todo setter R/O
    $Members=$Members|Group-object MemberType -AsHashTable -AsString
-   $body="`tthrow 'Not implemented'"
+   $body="`t  throw 'Not implemented'"
     #Recherche les propriété indexées
    Foreach($PropertiesGroup in $Members.Property|Group Name){
     Foreach($Property in $PropertiesGroup.Group){
@@ -46,22 +46,27 @@
        }
     }
    }
-   Write-Output "`r`n `r`n   # $TypeName`r`n"
+   [string]$Result=''
    Foreach($Method in $Members.Method){
-      $Ofs=",`r`n"
-      $Parameters="$(
-         Foreach ($Parameter in $Method.GetParameters())
-         {
-            Write-Output ('[{0}] ${1}' -f $Parameter.ParameterType,$Parameter.Name)
-         }
-      )"
-
-      Write-Output ("[{0}] {1}($Parameters){{`r`n$Body`r`n}}`r`n`r`n" -f $Method.ReturnType,$Method.Name)
+      $oldOfs,$Ofs=$ofs,",`r`n"
+      $Parameters= Foreach ($Parameter in $Method.GetParameters())
+        {
+          "`t`t[{0}] `${1}" -f $Parameter.ParameterType,$Parameter.Name
+        }
+      if ($Parameters.Count -ne 0)
+      {
+        $Ofs=$oldOfs
+        $result +="`r`n`t[{0}] {1}(`r`n$Parameters){{`r`n$Body`r`n`t}}`r`n" -f $Method.ReturnType,$Method.Name
+      }
+      else
+      { $result +="`r`n`t[{0}] {1}(){{`r`n$Body`r`n`t}}`r`n" -f $Method.ReturnType,$Method.Name }
    }
+   [string]$Result="`r`n# $TypeName"+$Result
+   Write-Output $Result
   }
   Else
   {
-   Write-Error "L’interface [$Type] contient un ou des événement. Son implémentation est impossible sous Powershell."
+   Write-Error "The interface [$Type] contains one or more events. Its implementation is impossible with Powershell."
   }
  }
 } #Get-InterfaceSignature
