9f09c5d9a40397558a7310d50636894711353327
LaurentDardenne <LaDardenne@Orange.fr>
2016-10-24 18:48:36 +0200

    Sript for publication (dev)

---
+++ Nuspec4Template.ps1
@@ -0,0 +1,37 @@
+﻿if(! (Test-Path variable:TemplateVcs))
+{ throw "The project configuration is required, see the 'Template_ProjectProfile.ps1' script." }
+
+$ModuleVersion=(Import-ManifestData "$TemplateVcs\Modules\Template\Template.psd1").ModuleVersion
+
+$Result=nuspec 'Template' $ModuleVersion {
+   properties @{
+        Authors='Dardenne Laurent'
+        Owners='Dardenne Laurent'
+        Description='PSScriptAnalyzer rules to validate the param block of a function.'
+        title='Template module'
+        summary='PSScriptAnalyzer rules to validate the param block of a function.'
+        copyright='Copyleft'
+        language='fr-FR'
+        licenseUrl='https://creativecommons.org/licenses/by-nc-sa/4.0/'
+        projectUrl='https://github.com/LaurentDardenne/Template'
+        #iconUrl='https://github.com/LaurentDardenne/Template/blob/master/icon/Template.png'
+#todo
+        releaseNotes="$(Get-Content "$TemplateVcs\Modules\Template\CHANGELOG.md" -raw)"
+        tags='Template'
+   }
+
+   dependencies {
+        dependency Log4Posh 2.0.0
+   }
+
+   files {
+        file -src "$TemplateVcs\Modules\Template\Template.psd1"
+        file -src "$TemplateVcs\Modules\Template\Template.psm1"
+        #file -src "$TemplateVcs\Modules\Template\README.md"
+        #file -src "$TemplateVcs\Modules\Template\releasenotes.md"
+   }
+}
+
+$Result|
+  Push-nupkg -Path $PSScriptAnalyzerRulesDelivery -Source 'https://www.myget.org/F/ottomatt/api/v2/package'
+
---
+++ Template_ProjectProfile.ps1
@@ -0,0 +1,48 @@
+﻿Param (
+ # Specific to the development computer
+ [string] $VcsPathRepository='' #"G:\PS\Plaster\TestProject\Out\Create"
+) 
+
+if (Test-Path env:APPVEYOR_BUILD_FOLDER)
+{
+  $VcsPathRepository=$env:APPVEYOR_BUILD_FOLDER
+}
+
+if (!(Test-Path $VcsPathRepository))
+{
+  Throw 'Configuration error, the variable $VcsPathRepository should be configured.'
+}
+
+# Common variable for development computers
+if ( $null -eq [System.Environment]::GetEnvironmentVariable('ProfileTemplate','User'))
+{ 
+ [Environment]::SetEnvironmentVariable('ProfileTemplate',$VcsPathRepository, 'User')
+  #refresh the Powershell environment provider
+ $env:ProfileTemplate=$VcsPathRepository 
+}
+
+ # Specifics variables  to the development computer
+$TemplateDelivery= "${env:Temp}\Delivery\Template"   
+$TemplateLogs= "{$env:Temp}\Logs\Template" 
+$TemplateDelivery, $TemplateLogs|
+ Foreach-Object {
+  new-item $_ -ItemType Directory -EA SilentlyContinue         
+ }
+
+ # Commons variable for all development computers
+ # Their content is specific to the development computer 
+$TemplateBin= "$VcsPathRepository\Bin"
+$TemplateHelp= "$VcsPathRepository\Documentation\Helps"
+$TemplateSetup= "$VcsPathRepository\Setup"
+$TemplateVcs= "$VcsPathRepository"
+$TemplateTests= "$VcsPathRepository\Tests"
+$TemplateTools= "$VcsPathRepository\Tools"
+$TemplateUrl='https://github.com/LaurentDardenne/Template' 
+
+ #PSDrive to the project directory 
+$null=New-PsDrive -Scope Global -Name Template -PSProvider FileSystem -Root $TemplateVcs 
+
+Write-Host 'Settings of the variables of Template project.' -Fore Green
+
+rv VcsPathRepository
+
