0f13613929a0c9f4b5cc04ad7ff83d078d8126f5
LaurentDardenne <LaDardenne@Orange.fr>
2017-04-30 16:23:27 +0200

    Delete spaces ( vscode ?)

--- Test/Edit-Template.Tests.ps1
+++ Test/Edit-Template.Tests.ps1
@@ -1,12 +1,8 @@
-﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
-. "$here\RCNewFiles.ps1"
-
-$here = $Here|Split-Path -Parent
-. "$here\$sut"
+﻿
+. "$PSScriptRoot\RCNewFiles.ps1"
 
 $PathSource="$Env:Temp\RC"
-. "$ProjectToolsVcs\Edit-Template.ps1"
+Import-Module  "..\Release\Template\Template.psd1"
 
 function normalizeEnds([string]$text)
 {
@@ -34,141 +30,140 @@ $ErrorCases=@(
 )
 
 function CompareStr{
- param([string[]]$Generate,[string[]]$Expected)         
- 
+ param([string[]]$Generate,[string[]]$Expected)
+
  $set1 = New-Object System.Collections.Generic.HashSet[String](,$Generate)
- return ( $set1.SetEquals($Expected) ) 
+ return ( $set1.SetEquals($Expected) )
 }
 
 
 Describe "Edit-Template" {
- 
+
  Context "When there is no error" {
-  
+
   It "basic principle -Clean directives"{
-    [string[]]$A=Get-Content -Path "$PathSource\Test0-1.ps1"  -ReadCount 0 -Encoding UTF8 | Edit-Template -Clean 
-    [string[]]$B=Get-Content -Path "$PathSource\Test0-1.new.ps1" -ReadCount 0 -Encoding UTF8        
-  
+    [string[]]$A=Get-Content -Path "$PathSource\Test0-1.ps1"  -ReadCount 0 -Encoding UTF8 | Edit-Template -Clean
+    [string[]]$B=Get-Content -Path "$PathSource\Test0-1.new.ps1" -ReadCount 0 -Encoding UTF8
+
     CompareStr $A $B| Should Be $true
-  } 
+  }
 
 
   It "basic principle -Clean directives"{
-    [string[]]$A=Get-Content -Path "$PathSource\Combination1.ps1"  -ReadCount 0 -Encoding UTF8 | Edit-Template -Clean 
-    [string[]]$B=Get-Content -Path "$PathSource\Combination1-Clean.ps1" -ReadCount 0 -Encoding UTF8        
-  
+    [string[]]$A=Get-Content -Path "$PathSource\Combination1.ps1"  -ReadCount 0 -Encoding UTF8 | Edit-Template -Clean
+    [string[]]$B=Get-Content -Path "$PathSource\Combination1-Clean.ps1" -ReadCount 0 -Encoding UTF8
+
     CompareStr $A $B| Should Be $true
-  } 
-    
-  
+  }
+
+
   It "basic principle -Remove directive"{
-    [string[]]$A=Get-Content -Path "$PathSource\Test0-2.ps1"  -ReadCount 0 -Encoding UTF8 | Edit-Template -Remove 
-    [string[]]$B=Get-Content -Path "$PathSource\Test0-2.new.ps1" -ReadCount 0 -Encoding UTF8        
-  
-    CompareStr $A $B| Should Be $true
-  } 
+    [string[]]$A=Get-Content -Path "$PathSource\Test0-2.ps1"  -ReadCount 0 -Encoding UTF8 | Edit-Template -Remove
+    [string[]]$B=Get-Content -Path "$PathSource\Test0-2.new.ps1" -ReadCount 0 -Encoding UTF8
 
- 
-  It "basic principle use 'Include' directive" {
-    [string[]]$A=Get-Content -Path "$PathSource\TestInclude1.ps1"  -ReadCount 0 -Encoding UTF8 | Edit-Template -Include 
-    [string[]]$B=Get-Content -Path "$PathSource\TestInclude1.new.ps1" -ReadCount 0 -Encoding UTF8        
-  
     CompareStr $A $B| Should Be $true
-  }   
+  }
+
+
+  It "basic principle use 'Include' directive" {
+    [string[]]$A=Get-Content -Path "$PathSource\TestInclude1.ps1"  -ReadCount 0 -Encoding UTF8 | Edit-Template -Include
+    [string[]]$B=Get-Content -Path "$PathSource\TestInclude1.new.ps1" -ReadCount 0 -Encoding UTF8
+
+    CompareStr $A $B| Should Be $true
+  }
 
   It "basic principle use 'Uncomment' directive" {
-    [string[]]$A=Get-Content -Path "$PathSource\TestUNCOMMENT.ps1"  -ReadCount 0 -Encoding UTF8 | Edit-Template -UnComment 
-    [string[]]$B=Get-Content -Path "$PathSource\TestUNCOMMENT.new.ps1" -ReadCount 0 -Encoding UTF8        
-  
+    [string[]]$A=Get-Content -Path "$PathSource\TestUNCOMMENT.ps1"  -ReadCount 0 -Encoding UTF8 | Edit-Template -UnComment
+    [string[]]$B=Get-Content -Path "$PathSource\TestUNCOMMENT.new.ps1" -ReadCount 0 -Encoding UTF8
+
     CompareStr $A $B| Should Be $true
-  }   
+  }
 
   It "basic principle -ConditionnalsKeyWord" -TestCase $TestCases {
     Param(
       [string]$File,
       [string]$New,
       [string]$Key
-     )  
-    Write-Host "key=$key" 
+     )
+    Write-Host "key=$key"
     [string[]]$A=Get-Content -Path $File -ReadCount 0 -Encoding UTF8 |
                   Edit-Template -ConditionnalsKeyWord $Key |
-                  Edit-Template -Clean 
-     
-    [string[]]$B=Get-Content -Path $New -ReadCount 0 -Encoding UTF8        
-  
+                  Edit-Template -Clean
+
+    [string[]]$B=Get-Content -Path $New -ReadCount 0 -Encoding UTF8
+
     CompareStr $A $B| Should Be $true
-  } 
-  
+  }
+
   It "basic principle -ConditionnalsKeyWord nested + combinations" -TestCase $TestCasesCombinations {
     Param(
       [string]$File,
       [string]$New,
       [string[]]$Key
-     )  
-    Write-Host "key=$key" 
+     )
+    Write-Host "key=$key"
     [string[]]$A=Get-Content -Path $File -ReadCount 0 -Encoding UTF8 |
                   Edit-Template -ConditionnalsKeyWord $Key
 
-    [string[]]$B=Get-Content -Path $New -ReadCount 0 -Encoding UTF8        
-  
-    CompareStr $A $B| Should Be $true                      
+    [string[]]$B=Get-Content -Path $New -ReadCount 0 -Encoding UTF8
+
+    CompareStr $A $B| Should Be $true
   }
  }
 
  Context "When there error" {
-  
+
   It "basic principle use 'Include' directive, file do not exist" {
     try {
       $ErrorActionPreference = "Stop"
-      [string[]]$A=Get-Content -Path "$PathSource\TestInclude2.ps1"  -ReadCount 0 -Encoding UTF8 | 
-        Edit-Template -Include 
-    } 
+      [string[]]$A=Get-Content -Path "$PathSource\TestInclude2.ps1"  -ReadCount 0 -Encoding UTF8 |
+        Edit-Template -Include
+    }
     catch
     {
-      $s="Include - the file do not exist '$PathSource\NotExist.ps1',Edit-Template"
       $_.FullyQualifiedErrorId | Should be 'IncludedFileNotFound,Edit-Template'
-    } 
-  }   
-  
+    }
+  }
+
   It "basic principle use 'Include' directive, Drive do not exist" {
     try {
       $ErrorActionPreference = "Stop"
-      [string[]]$A=Get-Content -Path "$PathSource\TestInclude3.ps1"  -ReadCount 0 -Encoding UTF8 | 
-        Edit-Template -Include 
-    } 
+      [string[]]$A=Get-Content -Path "$PathSource\TestInclude3.ps1"  -ReadCount 0 -Encoding UTF8 |
+        Edit-Template -Include
+    }
     catch
     {
       $_.FullyQualifiedErrorId | Should be 'IncludedFileNotFound,Edit-Template'
-    } 
-  }    
+    }
+  }
 
   It "basic principle -ConditionnalsKeyWord. Nesting 'DEFINE' erroneous "{
-   #peut importe la clé, la fonction valide l'ensemble des déclarations 
+   #peut importe la clé, la fonction valide l'ensemble des déclarations
     try {
      $ErrorActionPreference = "Stop"
      Get-Content -Path "$PathSource\Test01.ps1"  -ReadCount 0 -Encoding UTF8 |
       Edit-Template -ConditionnalsKeyWord 'A'
-    } 
+    }
     catch
     {
       $_.FullyQualifiedErrorId | Should be 'DirectivesIncorrectlyNested,Edit-Template'
-    } 
+    }
   }
-  
+
   It "basic principle -ConditionnalsKeyWord. Nesting 'UNDEF' erroneous "{
-   try { 
-    $ErrorActionPreference = "Stop"     
+   try {
+    $ErrorActionPreference = "Stop"
     Get-Content -Path "$PathSource\Test02.ps1"  -ReadCount 0 -Encoding UTF8 |
       Edit-Template -ConditionnalsKeyWord 'A'
    }
    catch
    {
      $_.FullyQualifiedErrorId | Should be 'DirectivesIncorrectlyNested,Edit-Template'
-   } 
+   }
    }
 
   It "basic principle -ConditionnalsKeyWord. Nesting 'DEFINE' orphan"{
-   try { 
+   try {
     $ErrorActionPreference = "Stop"
      Get-Content -Path "$PathSource\Test03.ps1"  -ReadCount 0 -Encoding UTF8 |
       Edit-Template -ConditionnalsKeyWord 'A'
@@ -176,8 +171,8 @@ Describe "Edit-Template" {
     catch
     {
       $_.FullyQualifiedErrorId | Should be 'DirectivesIncorrectlyNested,Edit-Template'
-    } 
-  }  
+    }
+  }
 
   It "basic principle -ConditionnalsKeyWord. 'DEFINE' is not nested, but orphan"{
    try {
@@ -188,20 +183,20 @@ Describe "Edit-Template" {
    catch
    {
      $_.FullyQualifiedErrorId | Should be 'IncompletDirective,Edit-Template'
-   } 
-  }  
+   }
+  }
 
   It "basic principle -ConditionnalsKeyWord. Nesting 'UNDEF' orphan"{
    try {
     $ErrorActionPreference = "Stop"
-    Get-Content -Path "$PathSource\Test04.ps1"  -ReadCount 0 -Encoding UTF8 | 
-      Edit-Template -ConditionnalsKeyWord 'A' 
+    Get-Content -Path "$PathSource\Test04.ps1"  -ReadCount 0 -Encoding UTF8 |
+      Edit-Template -ConditionnalsKeyWord 'A'
    }
    catch
    {
      $_.FullyQualifiedErrorId | Should be 'DirectivesIncorrectlyNested,Edit-Template'
-   } 
-  }  
+   }
+  }
 
   It "basic principle -ConditionnalsKeyWord. 'UNDEF' is not nested, but orphan"{
    try {
@@ -212,37 +207,37 @@ Describe "Edit-Template" {
    catch
    {
      $_.FullyQualifiedErrorId | Should be 'OrphanDirective,Edit-Template'
-   } 
-  } 
+   }
+  }
 
   It "basic principle -ConditionnalsKeyWord. 'UNDEF' missing" -Testcase $ErrorCases{
     Param(
       [string]$File,
       [string]$Key,
       [string] $FQE
-     )      
+     )
     Write-Host "`tFile : $File"
-   try { 
-    $ErrorActionPreference = "Stop" 
+   try {
+    $ErrorActionPreference = "Stop"
     Get-Content -Path $File  -ReadCount 0 -Encoding UTF8 |
       Edit-Template -ConditionnalsKeyWord $Key
    }
    catch
    {
      $_.FullyQualifiedErrorId | Should be $FQE
-   }      
-  } 
+   }
+  }
 
   It "basic principle -ConditionnalsKeyWord. 'DEFINE4 and 'UNDEF' are inverted" {
    try {
-    $ErrorActionPreference = "Stop" 
+    $ErrorActionPreference = "Stop"
     Get-Content -Path "$PathSource\Test09.ps1" -ReadCount 0 -Encoding UTF8 |
       Edit-Template -ConditionnalsKeyWord 'NotExist'
    }
    catch
    {
      $_.FullyQualifiedErrorId | Should be 'OrphanDirective,Edit-Template'
-   }         
-  } 
+   }
+  }
  }
 }
